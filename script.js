82e61a7b8e2c2b2a3b757afd40a9f70f048aa6aa
Yesenia Motola <yeseniamotola@gmail.com>
2022-07-25 22:37:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/*let desayuno = "café"
+
+if(desayuno == "café") {
+    console.log("Desayuno café con leche")
+} else {
+    console.log("No desayuno café con leche")
+}*/
+
+/*let desayuno = prompt("Ingrese su desayuno").toLowerCase();
+
+if(desayuno == "cafe") {
+    console.log("Desayuno café con leche")
+} else if(desayuno == "te") {
+    console.log("No desayuno café con leche, DESAYUNO TE")
+} else if (desayuno == "chocolatada") {
+    console.log("Desayuno chocolatada")
+} else {
+    console.log("No tenemos ese desayuno, disculpe.")
+}*/
+
+let numero = parseInt(prompt("Ingrese un número:"));
+
+if (numero % 2 == 0) {
+    console.log("El número es par uwu");
+} else {
+    console.log("El número es impar unu");
+}
+
+
