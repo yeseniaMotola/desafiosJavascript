886f22c68f009346cab730558eb8cbf83ff1a258
Yesenia Motola <yeseniamotola@gmail.com>
2022-07-28 06:15:12 -0300

    cambio de funcion, funcion credencial

--- script.js
+++ script.js
@@ -1,42 +1,14 @@
-function promedioEdadFamiliar(){
-    
-let cantFamiliares = parseInt(prompt("Ingrese la cantidad de personas que hay en su núcleo familiar:"));
 
-/*if (cantFamiliares === 0 ){
-    do {
-    cantFamiliares === parseInt(prompt("Por favor, ingrese un número válido"));
+let nombre;
+let apellido;
+let titulo;
 
-} while (isNaN(cantFamiliares));
-}*/
-
-let edad; 
-let sumaEdades = 0; //acumulador
-let i; //contador
-
-for (i=1; i<=cantFamiliares; i++){
-    edad = parseInt(prompt("Por favor, ingrese la edad del familiar n° " + i));
-
-    /*do {
-        edad = parseInt(prompt("Por favor, ingrese un número válido para la edad del familiar n° "  + i));
-    
-    } while (isNaN(edad));*/
-
-    sumaEdades = sumaEdades + edad;
-}
-
-alert(`La edad promedio de su núcleo familiar es de ${sumaEdades / cantFamiliares} años.`);
-}
-promedioEdadFamiliar()
-
-
-/*function cuidadoSegun(dia) {
-    if (dia === "lunes") {
-      return "fertilizar";
-    }  else  if (dia === "viernes") {
-      return "fumigar";
-    } else {
-      return "regar";
-    } 
+function credencial(titulo, nombre, apellido) {
+    prompt("Usted está por crear su credencial para ingresar al recinto. Para continuar presione enter")
+    nombre = prompt("Ingrese su nombre")
+    apellido = prompt("Ingrese su apellido")
+    titulo = prompt("Ingrese su titulo")
+    alert (` Esta es su credencial: ${titulo + " " + nombre + " " + apellido}`);
   }
-  */
-
+  
+  credencial()
