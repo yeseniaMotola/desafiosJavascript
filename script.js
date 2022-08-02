92f1ef664e2a6f699a4af800d894379df847976a
Yesenia Motola <yeseniamotola@gmail.com>
2022-08-01 23:56:14 -0300

    entrega arrays

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
+const michiBall1 = {
 
-let nombre;
-let apellido;
-let titulo;
+  nombre: "michi fresita",
+  precio: 200,
+  sabor: "Fresita",
+  nacimiento: "22/02/1998"
 
-function credencial(titulo, nombre, apellido) {
-    prompt("Usted está por crear su credencial para ingresar al recinto. Para continuar presione enter")
-    nombre = prompt("Ingrese su nombre")
-    apellido = prompt("Ingrese su apellido")
-    titulo = prompt("Ingrese su titulo")
-    alert (` Esta es su credencial: ${titulo + " " + nombre + " " + apellido}`);
-  }
-  
-  credencial()
+}
+
+const michiBall2 = {
+
+  nombre: "michi mandarinita",
+  precio: 125,
+  sabor: "Mandarinita",
+  nacimiento: "22/10/1999"
+
+}
+
+const michiBall3 = {
+
+  nombre: "michi naranjita",
+  precio: 250,
+  sabor: "Naranjita",
+  nacimiento: "22/03/2001"
+
+}
+
+const michiBall4 = {
+
+  nombre: "michi arandanito",
+  precio: 500,
+  sabor: "Arandanito",
+  nacimiento: "22/06/1996"
+
+}
+
+const michiBalls = [michiBall1, michiBall2, michiBall3, michiBall4]
+
+console.log(michiBalls)
+
