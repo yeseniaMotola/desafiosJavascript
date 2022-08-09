0fc53b996ab23272435cd248a72e7dbb8c032745
Yesenia Motola <yeseniamotola@gmail.com>
2022-08-08 23:26:14 -0300

    clean

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const michiBall1 = {
+/*const michiBall1 = {
 
   nombre: "michi fresita",
   precio: 200,
@@ -38,3 +38,72 @@ const michiBalls = [michiBall1, michiBall2, michiBall3, michiBall4]
 
 console.log(michiBalls)
 
+
+
+
+
+
+
+class usuario {
+
+  constructor(userName, password, email){
+    this.userName = userName
+    this.password = password
+    this.email = email
+  }
+
+}
+
+const usuarios = []
+
+const formulario = document.getElementById("formulario")
+
+formulario.addEventListener("submit", (evento) => {
+   
+ } )
+)
+
+
+
+
+//usuarios.push(new usuario ());*/
+
+
+
+class MichiBall{
+
+  constructor (etnia, antiguedad, sabor, vida){
+      this.etnia = etnia;
+      this.antiguedad = antiguedad;
+      this.sabor = sabor;
+      this.vida = vida
+    }
+
+    comunicarse = () => console.log(`${this.etnia} te dice "miau" `);
+    pedirAbrazo = () => console.log(`${this.etnia} necesita un abrazo o morirá :( `);
+} 
+
+
+const michiBall1 = new MichiBall("michi fresita", 1000, "Fresita", 5000);
+const michiBall2 = new MichiBall("michi mandarinita", 1300, "Mandarinita", 6000);
+const michiBall3 = new MichiBall("michi naranjita", 2000, "Naranjita", 5500);
+const michiBall4 = new MichiBall("michi arandanito", 3000, "Arandanito", 5800);
+const michiBall5 = new MichiBall("michi aguacatito", 2000000, "Aguacatito", 6800);
+
+const michiballs = [michiBall1, michiBall2, michiBall3, michiBall4, michiBall5]
+const etniasMichiballs = [" Fresita", " Mandarina", " Naranja", " Arándano", " Aguacate"]
+
+console.table(michiballs)
+
+alert(`Estas son las etnias conocidas de la raza alienígena "Michiballs": ${etniasMichiballs}`)
+
+/*michiballs.push("michi fresita")
+michiballs.push("michi mandarinita")
+
+console.log(michiballs)*/
+
+
+/*michiBall3.comunicarse()
+michiBall4.pedirAbrazo()
+michiBall2.comunicarse()
+michiBall5.pedirAbrazo()*/
