408cf742144779a9341e36a3f02ddb5fb1bbba85
Yesenia Motola <yeseniamotola@gmail.com>
2022-08-10 11:10:39 -0300

    practice

--- script.js
+++ script.js
@@ -1,109 +1,25 @@
-/*const michiBall1 = {
-
-  nombre: "michi fresita",
-  precio: 200,
-  sabor: "Fresita",
-  nacimiento: "22/02/1998"
-
-}
-
-const michiBall2 = {
-
-  nombre: "michi mandarinita",
-  precio: 125,
-  sabor: "Mandarinita",
-  nacimiento: "22/10/1999"
-
-}
-
-const michiBall3 = {
-
-  nombre: "michi naranjita",
-  precio: 250,
-  sabor: "Naranjita",
-  nacimiento: "22/03/2001"
-
-}
-
-const michiBall4 = {
-
-  nombre: "michi arandanito",
-  precio: 500,
-  sabor: "Arandanito",
-  nacimiento: "22/06/1996"
-
-}
-
-const michiBalls = [michiBall1, michiBall2, michiBall3, michiBall4]
-
-console.log(michiBalls)
 
 
-
-
-
-
-
-class usuario {
-
-  constructor(userName, password, email){
-    this.userName = userName
-    this.password = password
-    this.email = email
+class Grupo {
+  constructor(nombre, apellido, edad) {
+      this.nombre = nombre
+      this.apellido = apellido
+      this.edad = edad
   }
-
 }
 
-const usuarios = []
+const botonEnviar = document.getElementById("boton")
 
-const formulario = document.getElementById("formulario")
+let array = []
 
-formulario.addEventListener("submit", (evento) => {
-   
- } )
-)
+botonEnviar.addEventListener("click", (e) => {
+  e.preventDefault()
 
+  let nombre = document.getElementById("nombre").value
+  let apellido = document.getElementById("apellido").value
+  let edad = document.getElementById("numero").value
 
-
-
-//usuarios.push(new usuario ());
-
-
-
-class MichiBall{
-
-  constructor (etnia, antiguedad, sabor, vida){
-      this.etnia = etnia;
-      this.antiguedad = antiguedad;
-      this.sabor = sabor;
-      this.vida = vida
-    }
-
-    comunicarse = () => console.log(`${this.etnia} te dice "miau" `);
-    pedirAbrazo = () => console.log(`${this.etnia} necesita un abrazo o morirá :( `);
-} 
-
-
-const michiBall1 = new MichiBall("michi fresita", 1000, "Fresita", 5000);
-const michiBall2 = new MichiBall("michi mandarinita", 1300, "Mandarinita", 6000);
-const michiBall3 = new MichiBall("michi naranjita", 2000, "Naranjita", 5500);
-const michiBall4 = new MichiBall("michi arandanito", 3000, "Arandanito", 5800);
-const michiBall5 = new MichiBall("michi aguacatito", 2000000, "Aguacatito", 6800);
-
-const michiballs = [michiBall1, michiBall2, michiBall3, michiBall4, michiBall5]
-const etniasMichiballs = [" Fresita", " Mandarina", " Naranja", " Arándano", " Aguacate"]
-
-console.table(michiballs)
-
-alert(`Estas son las etnias conocidas de la raza alienígena "Michiballs": ${etniasMichiballs}`)
-
-michiballs.push("michi fresita")
-michiballs.push("michi mandarinita")
-
-console.log(michiballs)
-
-
-michiBall3.comunicarse()
-michiBall4.pedirAbrazo()
-michiBall2.comunicarse()
-michiBall5.pedirAbrazo()*/
+  const grupo1 = new Grupo(nombre, apellido, edad)
+  array.push(grupo1)
+  localStorage.setItem('alumno', JSON.stringify(array))
+})
