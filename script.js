7862f7d03f5fc34d0a25ffa74a78fce4dba749cf
Yesenia Motola <yeseniamotola@gmail.com>
2022-09-08 22:55:11 -0300

    proyecto

--- script.js
+++ script.js
@@ -1,134 +1,69 @@
+//Variables
+ portal = document.getElementById('portal');
+ michiball = document.getElementById('michiball');
+ puntos = document.querySelector('#puntos span');
+ contador = 0
 
+//funciones para mover el portal
 
-class Grupo {
-  constructor(nombre, apellido, edad) {
-      this.nombre = nombre
-      this.apellido = apellido
-      this.edad = edad
+function moveLeft(){
+  portalLeft = parseInt(window.getComputedStyle(portal).getPropertyValue('left'));
+  portalLeft -= 25
+  if(portalLeft >= -360){
+    portal.style.left = portalLeft + 'px';
   }
 }
 
-const botonEnviar = document.getElementById("boton")
+function moveRight(){
+  portalLeft = parseInt(window.getComputedStyle(portal).getPropertyValue('left'));
+  portalLeft += 25
+  if(portalLeft < 370){
+    portal.style.left = portalLeft + 'px';
+  }
+}
 
-let array = []
+//Mover el portal con teclas
 
-botonEnviar.addEventListener("click", (e) => {
-  e.preventDefault()
-
-  let nombre = document.getElementById("nombre").value
-  let apellido = document.getElementById("apellido").value
-  let edad = document.getElementById("numero").value
-
-  const grupo1 = new Grupo(nombre, apellido, edad)
-  array.push(grupo1)
-  localStorage.setItem('alumno', JSON.stringify(array))
+document.addEventListener("keydown", function(e){
+  if(e.key === "ArrowLeft"){
+    moveLeft()
+  }
+  if(e.key === "ArrowRight"){
+    moveRight()
+  }
 })
-/*const michiBall1 = {
 
-  nombre: "michi fresita",
-  precio: 200,
-  sabor: "Fresita",
-  nacimiento: "22/02/1998"
+//Generar michiballs aleatoriamente
 
-}
+setInterval(function(){
+  numeroAleatorio = Math.floor(Math.random() * 30) * 25
+  michiball.style.left = numeroAleatorio + 'px'
+  console.log(numeroAleatorio)
+},1600)
 
-const michiBall2 = {
+x = setInterval(function(){
+  
+  puntos.innerHTML = contador
 
-  nombre: "michi mandarinita",
-  precio: 125,
-  sabor: "Mandarinita",
-  nacimiento: "22/10/1999"
+  contador++;
 
-}
-
-const michiBall3 = {
-
-  nombre: "michi naranjita",
-  precio: 250,
-  sabor: "Naranjita",
-  nacimiento: "22/03/2001"
-
-}
-
-const michiBall4 = {
-
-  nombre: "michi arandanito",
-  precio: 500,
-  sabor: "Arandanito",
-  nacimiento: "22/06/1996"
-
-}
-
-const michiBalls = [michiBall1, michiBall2, michiBall3, michiBall4]
-
-console.log(michiBalls)
-
-
-
-
-
-
-
-class usuario {
-
-  constructor(userName, password, email){
-    this.userName = userName
-    this.password = password
-    this.email = email
-  }
-
-}
-
-const usuarios = []
-
-const formulario = document.getElementById("formulario")
-
-formulario.addEventListener("submit", (evento) => {
-   
- } )
-)
-
-
-
-
-//usuarios.push(new usuario ());
-
-
-
-class MichiBall{
-
-  constructor (etnia, antiguedad, sabor, vida){
-      this.etnia = etnia;
-      this.antiguedad = antiguedad;
-      this.sabor = sabor;
-      this.vida = vida
+  michiballTop = parseInt(window.getComputedStyle(michiballTop).getPropertyValue('top'))
+  portalTop = parseInt(window.getComputedStyle(portalTop).getPropertyValue('top'))
+  michiballLeft = parseInt(window.getComputedStyle(michiballLeft).getPropertyValue('left'))
+  portalLeft = parseInt(window.getComputedStyle(portalLeft).getPropertyValue('left'))
+  
+  if((michiballTop > 460) && (michiballTop < 500)){
+    if(portalLeft == michiballLeft){
+      
+    } else {
+      alert("GAME OVER");
+      clearInterval(x)
+      contador = 0
+      michiball.style.animation = 'none';
     }
-
-    comunicarse = () => console.log(`${this.etnia} te dice "miau" `);
-    pedirAbrazo = () => console.log(`${this.etnia} necesita un abrazo o morirá :( `);
-} 
+  }
+},10)
 
 
-const michiBall1 = new MichiBall("michi fresita", 1000, "Fresita", 5000);
-const michiBall2 = new MichiBall("michi mandarinita", 1300, "Mandarinita", 6000);
-const michiBall3 = new MichiBall("michi naranjita", 2000, "Naranjita", 5500);
-const michiBall4 = new MichiBall("michi arandanito", 3000, "Arandanito", 5800);
-const michiBall5 = new MichiBall("michi aguacatito", 2000000, "Aguacatito", 6800);
-
-const michiballs = [michiBall1, michiBall2, michiBall3, michiBall4, michiBall5]
-const etniasMichiballs = [" Fresita", " Mandarina", " Naranja", " Arándano", " Aguacate"]
-
-console.table(michiballs)
-
-alert(`Estas son las etnias conocidas de la raza alienígena "Michiballs": ${etniasMichiballs}`)
-
-michiballs.push("michi fresita")
-michiballs.push("michi mandarinita")
-
-console.log(michiballs)
 
 
-michiBall3.comunicarse()
-michiBall4.pedirAbrazo()
-michiBall2.comunicarse()
-michiBall5.pedirAbrazo()*/
